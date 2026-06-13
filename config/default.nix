e94bbf1ee3e7351c7ce85595f5aa6002772d3660
zwang695 <zwang695@users.noreply.github.com>
2026-06-13 14:37:55 -0700

    Add right trackpad macOS gestures

--- config/default.nix
+++ config/default.nix
@@ -4,8 +4,14 @@
 
 let
   config = ./.;
+  mouse_gesture_module = ../modules/zmk-mouse-gesture;
+  common = {
+    keymap = "${config}/go60.keymap";
+    kconfig = "${config}/go60.conf";
+    extraModules = [ mouse_gesture_module ];
+  };
 
-  go60_left  = firmware.zmk.override { board = "go60_lh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
-  go60_right = firmware.zmk.override { board = "go60_rh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
+  go60_left  = firmware.zmk.override (common // { board = "go60_lh"; });
+  go60_right = firmware.zmk.override (common // { board = "go60_rh"; });
 
 in firmware.combine_uf2 go60_left go60_right "go60"
